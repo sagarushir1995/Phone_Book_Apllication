d32583839394c6396e9fd3cbe0e4f6445473e44e
Sagar <sagarushir1995@gmail.com>
2022-08-18 15:52:47 +0530

    Phone book appliacation added

---
+++ src/main/java/com/phoneBooKApp/SpringPhoneBookApplication.java
@@ -0,0 +1,13 @@
+package com.phoneBooKApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPhoneBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPhoneBookApplication.class, args);
+	}
+
+}
